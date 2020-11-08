84130d0a0d18eaa8fa5ca08a22fe13442c28f120
Eric Vasilevsky <evasilevsky@gmail.com>
2020-11-08 14:03:28 -0700

    basic tile

---
+++ src/app/tile/tile.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ITile } from '../word-grid/word-grid.models';
+
+@Component({
+	selector: 'app-tile',
+	templateUrl: './tile.component.html',
+	styleUrls: ['./tile.component.scss']
+})
+export class TileComponent implements OnInit {
+	@Input()
+	tile: ITile;
+
+	@Output()
+	onTileSelected = new EventEmitter<ITile>();
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+}
