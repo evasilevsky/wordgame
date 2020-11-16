eff011ca8c9d33be390450e8081d50016fc5bb00
Diego Moura <diegoo.demoura@gmail.com>
2020-11-15 20:17:17 -0500

    Diego updates: services, constants and component

---
+++ src/app/constants.ts
@@ -0,0 +1,25 @@
+// Const declaration
+export const GRID_WIDTH: number = 20;
+export const GRID_HEIGHT: number = 20;
+export const GRID_SIZE: number = 20;
+export const DIRECTIONS: string[] = [
+  'horizontal',
+  'vertical',
+  'diagonal'
+]
+export const WORD_LIST: string[] = [
+  'Ditto',
+  'Arbok',
+  'Eevee',
+  'Entei',
+  'Gloom',
+  'Ekans',
+  'Pichu',
+  'Aipom',
+  'Duduo',
+  'Pikachu',
+  'bulbasaur',
+  'Squirtle',
+  'Charmander'
+];
+export const ALPHABET: string = 'abcdefghijklmnoprstuvwy'
--- src/app/word-grid/word-grid.component.ts
+++ src/app/word-grid/word-grid.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { EricsBoardGenerator } from './word-grid.models';
+import { GRID_SIZE, WORD_LIST } from '../constants';
+import { BoardService } from '../board.service';
+import { IBoardGenerator, ITile } from './word-grid.models'
 
 @Component({
 	selector: 'app-word-grid',
@@ -8,60 +10,13 @@ import { EricsBoardGenerator } from './word-grid.models';
 })
 
 export class WordGridComponent implements OnInit {
+	gameGrid: ITile[][];
+ 
 
-	gridSize = 15;
-	gridData = [];
+  constructor(private BoardService: BoardService) {
+  }
 
-	constructor() {
-		this.gridData = this.generateGridData(this.gridSize, this.getRandomLetter);
-	}
-
-	ngOnInit(): void {
-		const my = new EricsBoardGenerator();
-		this.gridData = my.generateBoard(this.gridSize, this.wordList);
-	}
-
-	generateGridData(gridSize, createOption) {
-		return [...Array(gridSize)].map(row => [...Array(gridSize)].map(opt => createOption()));
-	}
-
-	getRandomLetter() {
-		const alphabet = "abcdefghijklmnopqrstuvwxyz";
-		return alphabet[Math.floor(Math.random() * alphabet.length)];
-	}
-
-	selectItem($event) {
-		const item = $event.target.closest('.word-grid__item');
-		if (item) {
-			if ($event.type === 'click' || $event.key === 'Enter') {
-				const isSelected = item.classList.contains('selected');
-				item.setAttribute('aria-selected', !isSelected);
-				item.classList.toggle('selected');
-			}
-		}
-	}
-	handleKeyFocus($event) {
-		const row = $event.target.parentElement;
-		const rowIndex = row.getAttribute('data-index');
-		const item = $event.target.closest('.word-grid__item');
-		const nextTile = item.nextElementSibling;
-		const prevTile = item.previousElementSibling;
-
-		if (item) {
-			if ($event.key === 'ArrowLeft') {
-				if (prevTile) {
-					prevTile.focus();
-				} else if (rowIndex > 0) {
-					row.previousElementSibling.lastElementChild.focus();
-				}
-			}
-			if ($event.key === 'ArrowRight') {
-				if (nextTile) {
-					nextTile.focus();
-				} else if (rowIndex < this.gridSize - 1) {
-					row.nextElementSibling.firstElementChild.focus();
-				}
-			}
-		}
-	}
+  ngOnInit(): void {
+    this.gameGrid = this.BoardService.generateBoard(GRID_SIZE, WORD_LIST);
+  }  
 }
